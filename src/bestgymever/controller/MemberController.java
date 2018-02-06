8a527008eabdb69cc0d5aa6f4b56bcb364636dd9
frfr1701 <fredrik.frolund@yh.nackademin.se>
2018-02-06 13:55:29 +0100

    ---

--- src/bestgymever/controller/MemberController.java
+++ src/bestgymever/controller/MemberController.java
@@ -1,5 +1,22 @@
 package bestgymever.controller;
 
-public class MemberController {
+import bestgymever.models.*;
+
+
+public class MemberController implements IController{
+    
+    private final SuperModel model;
+    private State state;
+
+    public MemberController(SuperModel model, State state) {
+        this.model = model;
+        this.state = State.START;
+    }
+    
+
+    @Override
+    public SuperModel updateModel() {
+        return updateModel();
+    }
     
 }
